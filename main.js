e1ada1d5ad1b87a2be77bde31ef0aacefc84497c
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-17 20:21:34 +0100

    Add post request

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ const session = require("express-session")
 const app = express()
 const port = 4000
 
-app.use(session({}))
+app.use(session({secret: "secret"}))
 
 app.post("/set", (req, res) => {
-  debugger
+  console.log(req.query)
+  for (let key in req.params){
+    app.set(key, req.params[key])
+  }
+  res.send(`stored ${Object.keys(req.query).length}`)
 }) 
+
+// app.get("/get", (req, res) => {
+//   res.send('hello world')
+// })
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
 
