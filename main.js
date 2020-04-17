0a39e8933c8074c9e01c934e1a11e0cdb6ed2988
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-17 20:02:13 +0100

    Initialize express-session

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const session = require('express-session')
 const app = express()
 const port = 4000
+
+
+app.post("/set", (req, res) => {
+  debugger
+})  
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
 
